658513c28d6a517df31be8af7cd07426124db3a7
OctoY <69156675+OctoY@users.noreply.github.com>
2020-11-09 02:49:19 +0100

    Base Grid Rows can contain 12 or 16 columns, their function is to cancel widest colums padding so that spacing looks homogeneous
    Columns grow to fill the space, they maintain a width hierarchy with different flex-grow values and can have a max width

--- src/App.js
+++ src/App.js
@@ -4,7 +4,36 @@ import "./SASS/octoflex-ui.sass";
 export default function App() {
   return (
     <div className="App">
-      <h1>Octoflex UI</h1>
+    <div className="container">
+      <div className="row row-16">
+        <div className="col col-min-16">
+          <h1>Octoflex UI</h1>
+        </div>
+        <div className="col col-min-2">
+          <h1>2-X</h1>
+        </div>
+        <div className="col col-max-1">
+          <h1>1</h1>
+        </div>
+        <div className="col col-min-2">
+          <h1>2-X</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col col-min-12">
+          <h1>12</h1>
+        </div>
+        <div className="col col-min-2">
+          <h1>2-X</h1>
+        </div>
+        <div className="col col-max-1">
+          <h1>1</h1>
+        </div>
+        <div className="col col-min-2">
+          <h1>2-X</h1>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
