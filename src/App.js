fe908d38ba5175b71c61aedadc012bc76a271dae
OctoY <69156675+OctoY@users.noreply.github.com>
2020-11-10 00:59:09 +0100

    Positive Spacing + Dev Tools + New File Structure + .gitignore + -Margins + Padding from 1 to 5, space doubles with each size -Dev folder for dev-tools styles -Moved grid from _core to _grid and deleted _core

--- src/App.js
+++ src/App.js
@@ -4,36 +4,38 @@ import "./SASS/octoflex-ui.sass";
 export default function App() {
   return (
     <div className="App">
-    <div className="container">
-      <div className="row row-16">
-        <div className="col col-min-16">
-          <h1>Octoflex UI</h1>
+      <div className="container">
+        <div className="row row-16">
+          <div className="col col-min-16 p-0">
+            <div>
+              <h1>Octoflex UI</h1>
+            </div>
+          </div>
+          <div className="col col-min-2 px-0">
+            <div>2-X PX-0</div>
+          </div>
+          <div className="col col-max-1 ml-5">
+            <div>1 ML-5</div>
+          </div>
+          <div className="col col-min-2 mr-3">
+            <div>2-X MR-3</div>
+          </div>
         </div>
-        <div className="col col-min-2">
-          <h1>2-X</h1>
-        </div>
-        <div className="col col-max-1">
-          <h1>1</h1>
-        </div>
-        <div className="col col-min-2">
-          <h1>2-X</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col col-min-12">
-          <h1>12</h1>
-        </div>
-        <div className="col col-min-2">
-          <h1>2-X</h1>
-        </div>
-        <div className="col col-max-1">
-          <h1>1</h1>
-        </div>
-        <div className="col col-min-2">
-          <h1>2-X</h1>
+        <div className="row">
+          <div className="col col-min-12 pl-5">
+            <div>12 PL-5</div>
+          </div>
+          <div className="col col-min-2">
+            <div>2-X</div>
+          </div>
+          <div className="col col-max-1 p-0">
+            <div>1 P-0</div>
+          </div>
+          <div className="col col-min-2">
+            <div>2-X</div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
