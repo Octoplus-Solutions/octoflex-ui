120df5a37c096b4dc30d0dccd05f3f351ffa5bd2
OctoY <69156675+OctoY@users.noreply.github.com>
2020-11-10 03:29:00 +0100

    Prettyer dev colors & app.js + octoplus blue

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="header-row row-16">
-          <div className="col col-min-16 p-0">
+          <div className="col col-min-16 p-0 mb">
             <div>
               <h1>Octoflex UI</h1>
             </div>
           </div>
         </header>
-        <div className="row row-16">
+        <div className="row row-16 pt-3">
           <div className="col col-min-2">
             <div>2-X</div>
           </div>
@@ -85,8 +85,8 @@ export default function App() {
           <div className="col col-min-2">
             <div>2-X</div>
           </div>
-          <div className="col col-min-12 p-0">
-            <div>1 P-0</div>
+          <div className="col col-min-12 px-0">
+            <div>1 PX-0</div>
           </div>
           <div className="col col-max-2 mx-auto">
             <div>2-X MX-AUTO</div>
@@ -99,8 +99,8 @@ export default function App() {
           <div className="col col-min-2">
             <div>2-X</div>
           </div>
-          <div className="col col-min-12 p-0">
-            <div>1 P-0</div>
+          <div className="col col-min-12 px-0">
+            <div>1 PX-0</div>
           </div>
           <div className="col col-max-2 mx-auto">
             <div>2-X MX-AUTO</div>
