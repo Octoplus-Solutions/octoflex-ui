dc0c11dbb6f1abed44ccb0a1b0ba8a9fe9fd1279
OctoY <69156675+OctoY@users.noreply.github.com>
2020-11-10 07:02:03 +0100

    Demo page v.0.1

--- src/App.js
+++ src/App.js
@@ -8,24 +8,34 @@ export default function App() {
         <header className="header-row row-16">
           <div className="col col-min-16 p-0 mb">
             <div>
-              <h1>Octoflex UI</h1>
+              <h1>Octoflex UI 0.1</h1>
             </div>
           </div>
         </header>
-        <div className="row pt-3">
+        <div className="row pt-2">
           <div className="col col-min-2">
-            <div className="p-3">Hello :) I'm a .col-min-2, I will never take less than 2 columns but I will grow as long as there is space left for me.</div>
+            <div className="p-3">
+              Hello :) I'm a .col-min-2, I will never take less than 2 columns
+              but I will grow as long as there is space left for me.
+            </div>
           </div>
           <div className="col col-max-2">
-            <div className="p-3">I'm a .col-max-2, I will never grow more than 2 columns.</div>
+            <div className="p-3">
+              I'm a .col-max-2, I will never grow more than 2 columns.
+            </div>
           </div>
           <div className="col col-min-3">
-            <div className="p-3">I'm a .col-min-3, I will never be slimmer than 3 columns and I will grow as long as I find space. And with flex boxes magic I'm still bigger than my little sister .col-min-2, bigger col-min-* have a bigger flex-grow so that it will maintain width priority even when growing to fill empty space.  
+            <div className="p-3">
+              I'm a .col-min-3, I will never be slimmer than 3 columns and I
+              will grow as long as I find space. And with flex boxes magic I'm
+              still bigger than my little sister .col-min-2, bigger col-min-*
+              have a bigger flex-grow so that it will maintain width priority
+              even when growing to fill empty space.
             </div>
           </div>
         </div>
         <div className="row">
-        <div className="col col-max-1">
+          <div className="col col-max-1">
             <div></div>
           </div>
           <div className="col col-max-1">
@@ -63,7 +73,7 @@ export default function App() {
           </div>
         </div>
         <div className="row row-16">
-        <div className="col col-max-1">
+          <div className="col col-max-1">
             <div></div>
           </div>
           <div className="col col-max-1">
@@ -72,7 +82,7 @@ export default function App() {
           <div className="col col-max-1">
             <div></div>
           </div>
-          
+
           <div className="col col-max-1">
             <div>A</div>
           </div>
@@ -113,28 +123,44 @@ export default function App() {
             <div></div>
           </div>
         </div>
-        <div className="row row">
+        <div className="row pb-2">
           <div className="col col-min-10 offset-2">
             <div className="p-3">You can offset me.</div>
           </div>
           <div className="col col-min-3">
-            <div className="p-5">Use p-*, px-*, py-*, pl-*, pr-*, pt-*, pb-* classes for padding.</div>
+            <div className="p-5">
+              Use p-*, px-*, py-*, pl-*, pr-*, pt-*, pb-* classes for padding.
+            </div>
           </div>
           <div className="col col-min-3">
-            <div className="mt-5 p-3">Use m-*, mx-*, my-*, ml-*, mr-*, mt-*, mb-* classes for margins.</div>
+            <div className="mt-5 p-3">
+              Use m-*, mx-*, my-*, ml-*, mr-*, mt-*, mb-* classes for margins.
+            </div>
           </div>
           <div className="col col-min-3">
-            <div className="p-3 mb-n2">What about negative margins? Use m-n*, mx-n*, etc.</div>
+            <div className="p-3 mb-n2">
+              What about negative margins? Use m-n*, mx-n*, etc.
+            </div>
           </div>
           <div className="col col-min-3">
-            <div className="p-3 my-auto">And of course all the margin auto classes m-auto, mx-auto, etc.</div>
-          </div>        
+            <div className="p-3 my-auto">
+              And of course all the margin auto classes m-auto, mx-auto, etc.
+            </div>
+          </div>
+          <div className="col col-12">
+            <div className="p-3">The page Header is full width and sticky out of the box and the Footer sticks to the bottom even if the page is empty.</div>
+          </div>
+          <div className="col col-12">
+            <div id="blackbox" className="p-3"><p><strong>Those are only the foundations of Octoflex UI. All the new features will be presented on this demo page.</strong></p>
+            </div>
+          </div>
         </div>
         <footer className="footer-row row-16">
           <div className="col col-max-16 p-0">
             <div>
               <h3>
-                By&nbsp;
+              <a href="https://github.com/Octoplus-Solutions/octoflex-ui">Octoflex UI</a>
+              &nbsp;by&nbsp;
                 <a href="https://www.octoplus.solutions">Octoplus Solutions</a>
               </h3>
             </div>
